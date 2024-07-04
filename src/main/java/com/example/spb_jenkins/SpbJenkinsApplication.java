69daf8b5eebfa1d740c606db8a9dc4764bd9d600
SadwiReddy <124330579+SadwiReddy@users.noreply.github.com>
2024-07-04 15:38:09 +0530

    first commit

---
+++ src/main/java/com/example/spb_jenkins/SpbJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spb_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spb_jenkins/SpbJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spb_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
